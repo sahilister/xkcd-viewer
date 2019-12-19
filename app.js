e408486ff2f4e3e0622d419048b0c7d7cb3deca4
sahilister <sahil.dhiman27091999@gmail.com>
2019-12-19 09:51:05 +0530

    Add random button

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
+//homepage query, today's edition 
 const cors = 'https://nonanathi.herokuapp.com/';
-const url = 'https://xkcd.com/info.0.json';
-const json_data = `${cors}${url}`;
+let url = 'https://xkcd.com/info.0.json';
+let json_data = `${cors}${url}`;
+let max; //for rand
+let min = 1; //for rand
 var img_url;
+var title, edition;
+
+var img = document.createElement("img"); //defined globally to be used in random gen
+
 
 axios.get(json_data)
     .then(res => {
-        console.log(res.data);
         img_url = res.data.img;
-        
-        var img = document.createElement("img");
+        console.log(res.data);
+        title = res.data.title;
+        edition = res.data.num;
+
         img.src = img_url;
-        var src = document.getElementById("img-view");
+        var src = document.getElementById("img1");
         src.appendChild(img);
+        max = res.data.num; //maximun value for random xkcd generator
     });
 
+function randomImgGenerator() {
+    let rand = getRandomInt(min, max);
+    url = `https://xkcd.com/${rand}/info.0.json`
+    json_data = `${cors}${url}`; //making query url
+    
+    axios.get(json_data)
+    .then(res => {
+        img_url = res.data.img;
+        img.src = img_url;
+        var src = document.getElementById("img1");
+        src.appendChild(img);
+    });
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
